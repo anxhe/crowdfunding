4f646766efbee33337af4fe50587b42a191bdcf8
Anyelina Moreno Robledo <anyelinamoreno@gmail.com>
2017-12-07 12:09:39 +0100

    Add useMongoClient: true, in config/database

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,8 @@ const debug = require('debug')("angularauth:"+path.basename(__filename).split('.
 const mongoose = require('mongoose');
 const dbURL = process.env.DBURL || 'mongodb://localhost/crowdfunding';
 
-mongoose.connect(dbURL)
+mongoose.connect(dbURL, { useMongoClient: true })
+  // .then(() => require('./db-init')(server))
   .then(() => debug(`connected to database ${dbURL}`))
   .catch(e => {
     debug(`ERROR CONNECTING TO DB ${dbURL}`);
