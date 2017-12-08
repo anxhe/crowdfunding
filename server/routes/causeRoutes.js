38cfcef79e95c0d971102e2e6a3a40b6af2a1e7b
Anyelina Moreno Robledo <anyelinamoreno@gmail.com>
2017-12-08 11:16:38 +0100

    Change route /:causeId/members to put

--- server/routes/causeRoutes.js
+++ server/routes/causeRoutes.js
@@ -15,6 +15,6 @@ causeRoutes.get("/", paginate.middleware(10, 10), causeController.index);
 causeRoutes.get("/:id", causeController.show);
 causeRoutes.post("/", midle.isAuthenticated , upload.array('files'), causeController.create);
 causeRoutes.post("/:causeId/budget_item", causeBudgetItemController.create);
-causeRoutes.post("/:causeId/members", causeMemberController.edit);
+causeRoutes.put("/:causeId/members", causeMemberController.edit);
 
 module.exports = causeRoutes;
