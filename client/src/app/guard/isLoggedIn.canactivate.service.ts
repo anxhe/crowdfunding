64c7c5faef9e6b2c78b2ef30f0bdc8133b4bb201
Anyelina Moreno Robledo <anyelinamoreno@gmail.com>
2017-12-11 17:39:02 +0100

    Add router navigate inside isLoggedIn guard

--- client/src/app/guard/isLoggedIn.canactivate.service.ts
+++ client/src/app/guard/isLoggedIn.canactivate.service.ts
@@ -1,6 +1,7 @@
 import { CanActivate } from '@angular/router';
 import { Injectable }  from '@angular/core';
 import { Observable }  from 'rxjs/Rx';
+import { Router } from '@angular/router';
 
 import {AuthService} from '../services/auth.service';
 
@@ -10,14 +11,12 @@ const timeout = (nS) => new Promise((resolve) => setTimeout(resolve,nS * 1000));
 @Injectable()
 export class IsLoggedInService implements CanActivate {
 
-  constructor(private auth:AuthService) { }
+  constructor(private auth:AuthService, private router: Router) { }
 
   canActivate(): Observable<boolean> | Promise<boolean> | boolean {
     console.log("Checking can activate");
 
-    //return timeout(5).then(() => true);
-    //return this.auth.isLoggedIn().map(user => true)
-    return this.auth.getUser() ? true : false
-    //return false;
+    return this.auth.getUser() ? true :  this.router.navigate(['login'])
+
   }
 }
