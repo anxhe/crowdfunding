f3cc6479251c232cfd0caa04d9638ccccaacb815
Anyelina Moreno Robledo <anyelinamoreno@gmail.com>
2017-12-13 10:16:40 +0100

    Fix bug in CanActivate(guard)

--- client/src/app/guard/isLoggedIn.canactivate.service.ts
+++ client/src/app/guard/isLoggedIn.canactivate.service.ts
@@ -1,22 +1,26 @@
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Injectable }  from '@angular/core';
 import { Observable }  from 'rxjs/Rx';
-import { Router } from '@angular/router';
-
 import {AuthService} from '../services/auth.service';
 
 
-const timeout = (nS) => new Promise((resolve) => setTimeout(resolve,nS * 1000));
+// const timeout = (nS) => new Promise((resolve) => setTimeout(resolve,nS * 1000));
 
 @Injectable()
 export class IsLoggedInService implements CanActivate {
 
   constructor(private auth:AuthService, private router: Router) { }
 
-  canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+  canActivate(next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     console.log("Checking can activate");
 
-    return this.auth.getUser() ? true :  this.router.navigate(['login'])
+    const isAuth = this.auth.getUser() ? true : false
+
+    if(!isAuth){
+      this.router.navigate(['login']);
+    }
+    return isAuth
 
   }
 }
